281c4f21271d177aa6e1a9a1f69d5fc993e7498d
Marcos Ribeiro <mrdezigner@gmail.com>
2016-05-12 18:32:33 -0300

    Update 14 - CAMPO PERSONALIZADO.php

--- 14 - CAMPO PERSONALIZADO.php
+++ 14 - CAMPO PERSONALIZADO.php
@@ -4,3 +4,6 @@
 
 <?php $imagem = get_post_meta($post->ID, 'imagem', true); ?>
 <img src="<?php echo $imagem; ?>"
+
+<!-- IMAGEM DESTACADA -->
+<img class="img-responsive" src="<?php echo get_settings('home'); ?>/<?php $key="img"; echo get_post_meta(get_the_ID(),'img',true); ?>" alt="<?php the_title(); ?>" border="0" />
