d69ab92c5cb7647bbebfd20e94e6822cc46d980c
Marcos Ribeiro <mrdezigner@gmail.com>
2016-06-09 09:44:19 -0300

    Update

--- 22 - Disqus.php
+++ 22 - Disqus.php
@@ -20,3 +20,20 @@ function disqus_embed($disqus_shortname) {
 // inserir o código <?php disqus_embed(‘meublog’); ?> 
 // tanto no ficheiro single.php como no ficheiro page.php para introduzir os comentários.
 // 'meublog' se referi ao id de cadastro no site da Disqus: www.disqus.com
+
+/*
+* Para exibir a contagem de comentários, mais uma função curta (na functions, claro).
+* disqus_shortname = o nome de cadastro no Disqus
+*/
+
+function disqus_count($disqus_shortname) {	
+wp_enqueue_script('disqus_count','http://'.$disqus_shortname.'.disqus.com/count.js');
+
+echo '<a href="'. get_permalink() .'#disqus_thread"></a>';
+}
+
+/*
+* E onde quiser exibir a contagem:
+*/
+
+<?php disqus_count('shortname-do-seu-blog'); ?>
