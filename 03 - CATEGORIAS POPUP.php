0aae3a434b632fb310c17125ceb376013fd58a39
Marcos Ribeiro <mrdezigner@gmail.com>
2016-11-28 11:47:38 -0300

    Update - Novos recursos

--- 03 - CATEGORIAS POPUP.php
+++ 03 - CATEGORIAS POPUP.php
@@ -36,6 +36,39 @@ echo trim($output, $separator);
 * Lembrando que você deve alterar o valor 1 para o ID da categoria desejada.
 */
 
+
+/* Para pegar o título da categoria dentro do Loop use: */
+<?php
+$categoria = get_the_category();
+$nomeCategoria = $categoria[0]->cat_name;
+echo $nomeCategoria;
+?>
+
+Para pegar o título da categoria fora do Loop use:
+
+<?php
+global $post;
+$categoria = get_the_category($post->id);
+$nomeCategoria = $categoria[0]->cat_name;
+echo $nomeCategoria;
+?>
+
+/* Para pegar as subcategorias (categoria filho) mudeo o zero para um: */
+
+<?php $subcategoria = get_the_category(); $nomesubCategoria = $categoria[1]->cat_name; ?>
+
+/* Se estiver usando um Custom Post (Post personalizado) que possua Custom Taxonomy
+ * (Categorias do psot personalizado) use o comando abaixo para exibir o nome da taxonomy:
+*/
+
+<?php global $post;
+$terms = get_the_terms($post->id, 'AQUI VOCÊ COLOCA O NOME DA TAXONOMIA');
+$nome_taxonomy = $terms[0]->name;
+echo $nome_taxonomy;
+?>
+
+/* */
+
 // Referência:
 
 http://codex.wordpress.org/Template_Tags/get_the_category
